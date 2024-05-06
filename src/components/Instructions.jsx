baf2d4c20e9e7ad4d1b0692700724f6be3c2e7fa
gsampao <gnsampao@gmail.com>
2024-05-06 19:51:48 +0300

    Fixed lists alignment.

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,44 +1,44 @@
 const Instructions = () => {
   return (
-    <section className="w-[312px] m-auto mb-6 border-b-2 border-b-light-grey">
+    <section className="w-[304px] m-auto mb-6 border-b-2 border-b-light-grey">
       <h2 className="text-nutmeg font-serif text-3xl mb-4">Instructions</h2>
-      <ol className="list-decimal ml-6 text-wenge-brown w-72">
+      <ol className="list-decimal ml-6 text-nutmeg font-bold w-72">
         <li className="mb-2">
-          <p className="pl-4">
+          <p className="pl-4 text-wenge-brown font-normal">
             <b>Beat the eggs: </b>In a bowl, beat the eggs with a pinch of salt
             and pepper until they are well mixed. You can add a tablespoon of
             water or milk for a fluffier texture.
           </p>
         </li>
         <li className="mb-2">
-          <p className="pl-4">
+          <p className="pl-4 text-wenge-brown font-normal">
             <b>Heat the pan: </b>Place a non-stick frying pan over medium heat
             and add butter or oil.
           </p>
         </li>
         <li className="mb-2">
-          <p className="pl-4">
+          <p className="pl-4 text-wenge-brown font-normal">
             <b>Cook the omelette: </b>Once the butter is melted and bubbling,
             pour in the eggs. Tilt the pan to ensure the eggs evenly coat the
             surface.
           </p>
         </li>
         <li className="mb-2">
-          <p className="pl-4">
+          <p className="pl-4 text-wenge-brown font-normal">
             <b>Add fillings (optional): </b>When the eggs begin to set at the
             edges but are still slightly runny in the middle, sprinkle your
             chosen fillings over one half of the omelette.
           </p>
         </li>
         <li className="mb-2">
-          <p className="pl-4">
+          <p className="pl-4 text-wenge-brown font-normal">
             <b>Fold and serve: </b>As the omelette continues to cook, carefully
             lift one edge and fold it over the fillings. Let it cook for another
             minute, then slide it onto a plate.
           </p>
         </li>
         <li className="pb-6">
-          <p className="pl-4">
+          <p className="pl-4 text-wenge-brown font-normal">
             <b>Enjoy: </b>Serve hot, with additional salt and pepper if needed.
           </p>
         </li>
--- src/components/Preparation.jsx
+++ src/components/Preparation.jsx
@@ -1,23 +1,23 @@
 const Preparation = () => {
   return (
-    <section className="bg-rose-white pt-6 pb-6 mt-6 mb-6 rounded-lg w-[312px]">
-      <h3 className="text-dark-raspberry font-sans text-xl font-bold mb-2 ml-8">
+    <section className="bg-rose-white pt-6 pb-6 mt-6 mb-6 rounded-lg w-[304px]">
+      <h3 className="text-dark-raspberry font-sans text-xl font-bold mb-2 pl-6">
         Preparation time
       </h3>
-      <ul className="list-disc list-inside w-60 text-wenge-brown ml-8 grid">
+      <ul className="list-disc pl-12 text-dark-raspberry">
         <li className="mb-2">
-          <p className="inline">
-            <b className="pl-6">Total: </b>Approximately 10 minutes
+          <p className="pl-4 w-60 text-wenge-brown">
+            <b>Total: </b>Approximately 10 minutes
           </p>
         </li>
         <li className="mb-2">
-          <p className="inline">
-            <b className="pl-6">Preparation: </b>5 minutes
+          <p className="pl-4 w-60 text-wenge-brown">
+            <b>Preparation: </b>5 minutes
           </p>
         </li>
         <li>
-          <p className="inline">
-            <b className="pl-6">Cooking: </b>5 minutes
+          <p className="pl-4 w-60 text-wenge-brown">
+            <b>Cooking: </b>5 minutes
           </p>
         </li>
       </ul>
