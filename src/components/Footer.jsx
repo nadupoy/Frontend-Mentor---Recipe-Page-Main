951543c0263aa7cfd4ee9e0e85a30093cee875f9
gsampao <gnsampao@gmail.com>
2024-05-17 15:16:59 +0300

    Added styling for mobile devices.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,22 @@ import { links } from "../constants";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>
+    <footer className="text-center text-xs mb-4">
+      <p className="text-wenge-brown">
         Challenge by{" "}
-        <a href={links.frontendMentor} target="_blank">
+        <a
+          href={links.frontendMentor}
+          target="_blank"
+          className="text-nutmeg no-underline hover:cursor-pointer hover:font-bold visited:text-nutmeg visited:no-underline"
+        >
           Frontend Mentor
         </a>
         . Coded by{" "}
-        <a href={links.gitHub} target="_blank">
+        <a
+          href={links.gitHub}
+          target="_blank"
+          className="text-nutmeg no-underline hover:cursor-pointer hover:font-bold visited:text-nutmeg visited:no-underline"
+        >
           gsampao
         </a>
         .
